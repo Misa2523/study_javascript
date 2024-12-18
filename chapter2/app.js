5baf82b9c9e2082294a876da86845a8340ad6767
Misa2523 <sa5chsgrnccon2510@gmail.com>
2024-12-18 15:03:22 +0000

    [Add]四則演算/文字列の結合

--- chapter2/app.js
+++ chapter2/app.js
@@ -1,2 +1,28 @@
 let hello = 'Hello World'; // let 変数名 = 内容;
 alert(hello);
+
+
+/* その他の書き方 （それぞれの記述の後にalert(変数名);を追記すれば、アラートウィンドウに指定した値が表示される）*/
+let int1 = 1; // 整数を代入
+
+let int2 = -10; // 負数を代入
+
+let float1 = 3.14;  // 小数点を代入
+
+let str1 = 'JavaScriptを覚えよう';  // 文字列を代入
+
+
+/* 四則演算の書き方 */
+alert(4 + 3);
+alert(8 - 5);
+alert(2 * 6);
+alert(10 / 2);
+
+
+/* 文字列を結合する */
+alert('Hello' + 'World');
+
+/* 変数を利用した文字列の結合 */
+let str2 = 'Hello'
+let str3 = 'World!!'
+alert(str2 + str3);
