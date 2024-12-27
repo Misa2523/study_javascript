6d8096693bfed2676387b7cba5e21021ddd45cbf
Misa2523 <sa5chsgrnccon2510@gmail.com>
2024-12-27 18:23:03 +0000

    [Finish]イベントを検出する(chapter6)

--- jQuery-Lesson/chapter6/app.js
+++ jQuery-Lesson/chapter6/app.js
@@ -35,17 +35,35 @@ $('.セレクタ名').イベント名(function(){
 
 
 // class属性を追加する/解除する
-$(function(){
-  $('.box1').mouseover(function(){
-     $('.box1').addClass('box1-ext'); //マウスオーバーしたら.box1-extのclassが追加される
-  });
-  $('.box1').mouseout(function(){
-     $('.box1').removeClass('box1-ext'); //マウスアウトしたら.box1-extのclassが外れる
-  });
-});
+// $(function(){
+//   $('.box1').mouseover(function(){
+//     $('.box1').addClass('box1-ext'); //マウスオーバーしたら.box1-extのclassが追加される
+//   });
+//   $('.box1').mouseout(function(){
+//     $('.box1').removeClass('box1-ext'); //マウスアウトしたら.box1-extのclassが外れる
+//   });
+// });
 
 /*addClass( )では、対象のHTML要素にclass属性を追加する
 removeClass( )では、要素に設定されているclass属性が解除される*/
 
 /*----------------------------「マウスクリック」イベントを設定する----------------------------*/
+/*
+マウスクリックのイベントには、.click()もあるが、これはクリックされた時点でイベントが終了してしまう。
+クリック後に複数のイベントが行われるようにするため、onをつけた以下の記述を使う。
+
+【マウスクリックイベントの記述】
+$('.セレクタ名').on('click', function(){
+  イベント発生時に行われる処理
+});
+*/
+
+$(function(){
+  $('.box1').on('click', function(){
+     $('.box1').addClass('box1-ext'); //.box1がクリックされたら、box1-extのclassを追加
+  });
+  $('.box1').mouseout(function(){
+     $('.box1').removeClass('box1-ext'); //マウスアウトされたら、box1-extのclassを削除
+  });
+});
 
